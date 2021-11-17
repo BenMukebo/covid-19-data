29699ca4fe07953b7658d4b00dd219d793bb0f22
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-17 09:24:54 +0200

    Implement React router for my pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/ConfigureStore';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/:id">
+            <Details />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
