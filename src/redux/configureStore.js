18f10d656b96372a2d43a676328c7c5a12c04d72
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-17 08:30:08 +0200

    Add configureStore.js file

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const reducer = combineReducers({
+
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk),
+);
+
+export default store;
