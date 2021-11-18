bbb21da46c782537dbd0bb8135433d2c877ace79
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-18 07:09:31 +0200

    Modify the Api.js file

--- src/redux/countries/api.js
+++ src/redux/countries/api.js
@@ -1,13 +1,22 @@
-const getContinent = async () => {
-  const request = await fetch('https://covid-api.mmediagroup.fr/v1/cases?continent=africa');
-  const countries = await request.json();
-  return countries;
+
+const BASE_URL = 'https://covid-api.mmediagroup.fr/v1/cases';
+
+export const getContinent = async () => {
+  // const request = await fetch('https://covid-api.mmediagroup.fr/v1/cases?continent=europe');
+  // const countries = await request.json();
+  // return countries;
+  const continent = 'europe'
+  const request = await fetch(`${BASE_URL}?continent=${continent}`);
+  const response = await request.json();
+  return response;
 };
 
-const getCountry = async (country) => {
-  const request = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?country=${country}`);
-  const result = await request.json();
-  return result;
+export const getCountry = async (country) => {
+  // const request = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?country=${country}`);
+  const response = await fetch(`${BASE_URL}?country=${country}`);
+  const response = await request.json();
+  return response;
+  
 };
 
-export { getContinent, getCountry };
+// export { getContinent, getCountry };
