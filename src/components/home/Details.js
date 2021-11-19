759e20684f94880e98b639c2b266017fe525f676
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-19 20:23:16 +0200

    Displaying  details of a contry

--- src/components/home/Details.js
+++ src/components/home/Details.js
@@ -7,13 +7,11 @@ import {
   faAngleLeft,
   faCog,
   faMicrophoneAlt,
-  // faGlobeEurope,
 } from '@fortawesome/free-solid-svg-icons';
 import { fetchCountry } from '../../redux/countries/countries';
 import formatNumber from '../utils/formatNumber';
-// import Icon from '../utils/Icon';
 import image from '../../images/covid.svg';
-// import styles from '../css/Details.module.css';
+import styles from '../css/details.module.css';
 
 const Details = () => {
   const { name } = useParams();
@@ -32,49 +30,80 @@ const Details = () => {
   }
 
   const { All } = country;
-  const list = Object.entries(country).slice(1);
+  // console.log(All);
+  const list = Object.entries(country).slice();
+  console.log(list);
 
   return (
     <section>
-      <header className="header">
-        <Link to="/">
-          {/* <Icon name="arrow_back_ios" /> */}
-          <FontAwesomeIcon icon={faAngleLeft} className="fontawesome" />
-        </Link>
-        <h5 className="header-title">town/city views</h5>
-        {/* <Icon name="mic" /> */}
-        <FontAwesomeIcon icon={faMicrophoneAlt} />
-        <div className="pl-5">
-          {/* <Icon name="settings" /> */}
-          <FontAwesomeIcon icon={faCog} />
+      <header className={styles.header}>
+        <span className={styles.left}>
+          <Link to="/">
+            <FontAwesomeIcon icon={faAngleLeft} className="fontawesome" />
+          </Link>
+        </span>
+        <h4 className={styles['header-title']}>town/city views</h4>
+        <div className={styles['right-icons']}>
+          <span>
+            <FontAwesomeIcon icon={faMicrophoneAlt} />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faCog} />
+          </span>
         </div>
       </header>
-      <div className="Details">
-        <div className="Details-banner-left">
-          <img src={image} alt="map view" className="App-map" />
+
+      <div className={styles['main-details']}>
+        <div className={styles['details-img']}>
+          <img src={image} alt="virus-img" className="App-map" />
         </div>
-        <div className="Details-banner-right">
-          <h1 className="App-title">{All.country}</h1>
-          <p className="App-subtitle">
+        <div className={styles['header-title']}>
+          <h1>{All.country}</h1>
+          <p>
             {`${formatNumber(
               All.confirmed,
             )} cases`}
           </p>
         </div>
       </div>
+
       <section className="Home-stats">
         <h4 className="App-section-title">Twon/City cases</h4>
         <ul>
-          {list.map(([name, { confirmed }]) => (
+          {list.map(([name, {
+            population, location, abbreviation, recovered, confirmed, deaths, updated,
+          }]) => (
             <li key={name} className="Details-item">
-              <h6 className="Details-item-title">{name}</h6>
-              <div className="Details-item-right">
-                <p className="App-subtitle">
-                  {formatNumber(confirmed)}
-                  {' '}
-                  cases
-                </p>
-                {/* <Icon name="arrow_circle_right" /> */}
+              <div>
+                <div>
+                  <h5>population</h5>
+                  <p>{formatNumber(population)}</p>
+                </div>
+                <div>
+                  <h5>location</h5>
+                  <p>{formatNumber(location)}</p>
+                </div>
+                <div>
+                  <h5>capital_city</h5>
+                  <p>{formatNumber(abbreviation)}</p>
+                </div>
+                <div>
+                  <h5>recovered</h5>
+                  <p>{formatNumber(recovered)}</p>
+                </div>
+                <div>
+                  <h5>confirmed</h5>
+                  <p>{formatNumber(confirmed)}</p>
+                </div>
+
+                <div>
+                  <h5>deaths</h5>
+                  <p>{formatNumber(deaths)}</p>
+                </div>
+                <div>
+                  <h5>Last updated</h5>
+                  <p>{formatNumber(updated)}</p>
+                </div>
                 <FontAwesomeIcon icon={faArrowCircleRight} />
               </div>
             </li>
@@ -86,3 +115,18 @@ const Details = () => {
 };
 
 export default Details;
+
+// abbreviation: "ZA"
+// capital_city: "Pretoria"
+// confirmed: 2926914
+// continent: "Africa"
+// country: "South Africa"
+// deaths: 89515
+// elevation_in_meters: "1,034"
+// iso: 710
+// lat: "-30.5595"
+// life_expectancy: "51.1"
+// location: "Southern Africa"
+// long: "22.9375"
+// population: 56717156
+// recovered: 0
