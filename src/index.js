ed971bb9ec885993624d7f8ac925b9c5d06b3daf
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-20 20:59:52 +0200

    Remove the eslint disable line and fix the issue in the code.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './redux/configureStore';
-// import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
